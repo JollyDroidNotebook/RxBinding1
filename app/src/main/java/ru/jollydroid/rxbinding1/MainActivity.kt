07f34c21da10e0a7c8ddfb03d3756dc5cde4979e
Igor Tceglevskii <igor.tceglevskii@agoda.com>
2019-04-16 10:22:08 +0700

    cleanup

--- app/src/main/java/ru/jollydroid/rxbinding1/MainActivity.kt
+++ app/src/main/java/ru/jollydroid/rxbinding1/MainActivity.kt
@@ -1,17 +1,14 @@
 package ru.jollydroid.rxbinding1
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.widget.textChanges
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.Observables
 import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.Toast
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,52 +16,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        button.isEnabled = false
-//        button.isClickable = false
 
-        val isEmailValid = email
-            .textChanges()
+        val isEmailValid = email.textChanges()
             .map { t -> EMAIL_ADDRESS.matcher(t).matches() }
             .distinctUntilChanged()
 
-        val isPasswordValid = password
-            .textChanges()
+        val isPasswordValid = password.textChanges()
             .map { t -> (t.length > 4) }
             .distinctUntilChanged()
 
         isPasswordValid
-            .doOnNext { Log.d("happy", "password $it") }
             .map { b -> if (b) Color.BLACK else Color.RED }
-            .subscribe { c ->
-                password.setTextColor(c)
-            }
+            .subscribe { c -> password.setTextColor(c) }
 
         isEmailValid
-            .doOnNext { Log.d("happy", "email $it") }
             .map { b -> if (b) Color.BLACK else Color.RED }
             .subscribe { c -> email.setTextColor(c) }
 
         // note                vvvvvvvvvvv: Observables (from RxKotlin), not Observable
-        val isSignUpPossible =
-            Observables
+        val isSignUpPossible = Observables
                 .combineLatest(isEmailValid, isPasswordValid) { e, p -> e && p }
                 .distinctUntilChanged()
 
         isSignUpPossible
             .subscribe { b ->
-                Log.d("happy", "button $b")
                 button.isEnabled = b
                 button.isClickable = b
             }
 
-        button
-            .clicks()
-            .doOnNext { Log.d("happy", "click") }
+        button.clicks()
             .subscribe { signUpButtonAction() }
     }
 
-    private fun signUpButtonAction() {
+    private fun signUpButtonAction() =
         Toast.makeText(this@MainActivity, "Button clicked", Toast.LENGTH_LONG).show()
-    }
-
 }
