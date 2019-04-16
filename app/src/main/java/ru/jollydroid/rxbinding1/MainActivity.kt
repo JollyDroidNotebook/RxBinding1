f633114b03d0fc6ec45537489569273185c4be44
Igor Tceglevskii <igor.tceglevskii@agoda.com>
2019-04-16 11:28:07 +0700

    unsubscribe by lifecycle

--- app/src/main/java/ru/jollydroid/rxbinding1/MainActivity.kt
+++ app/src/main/java/ru/jollydroid/rxbinding1/MainActivity.kt
@@ -1,19 +1,32 @@
 package ru.jollydroid.rxbinding1
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
+import androidx.lifecycle.Lifecycle
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.widget.textChanges
+import com.trello.lifecycle2.android.lifecycle.AndroidLifecycle
+import com.trello.rxlifecycle3.kotlin.bindToLifecycle
+import com.trello.rxlifecycle3.kotlin.bindUntilEvent
+import io.reactivex.Observable
 import io.reactivex.rxkotlin.Observables
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
+        val lifecycleProvider = AndroidLifecycle.createLifecycleProvider(this)
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -27,25 +40,47 @@ class MainActivity : AppCompatActivity() {
 
         isPasswordValid
             .map { b -> if (b) Color.BLACK else Color.RED }
+            .bindToLifecycle(lifecycleProvider)
             .subscribe { c -> password.setTextColor(c) }
 
         isEmailValid
             .map { b -> if (b) Color.BLACK else Color.RED }
+            .bindToLifecycle(lifecycleProvider)
             .subscribe { c -> email.setTextColor(c) }
 
         // note                vvvvvvvvvvv: Observables (from RxKotlin), not Observable
         val isSignUpPossible = Observables
-                .combineLatest(isEmailValid, isPasswordValid) { e, p -> e && p }
-                .distinctUntilChanged()
+            .combineLatest(isEmailValid, isPasswordValid) { e, p -> e && p }
+            .distinctUntilChanged()
 
         isSignUpPossible
+            .bindToLifecycle(lifecycleProvider)
             .subscribe { b ->
                 button.isEnabled = b
                 button.isClickable = b
             }
 
         button.clicks()
+            .bindToLifecycle(lifecycleProvider)
             .subscribe { signUpButtonAction() }
+
+        val ticker = Observable
+            .interval(1, TimeUnit.SECONDS, Schedulers.io())
+            .share()
+
+        ticker
+            .bindToLifecycle(lifecycleProvider)
+            .subscribe { o -> Log.d("happy", "tick " + o) }
+
+        lifecycleProvider.lifecycle()
+            .bindToLifecycle(lifecycleProvider)
+            .filter { it == Lifecycle.Event.ON_RESUME }
+            .subscribe {
+                ticker
+                    .bindUntilEvent(lifecycleProvider, Lifecycle.Event.ON_PAUSE)
+                    .subscribe { o -> Log.d("happy", "tack " + o) }
+            }
+
     }
 
     private fun signUpButtonAction() =
